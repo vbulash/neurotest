25b3883f4c9887911cb73dbf8d033156f6742558
vbulash <vbulash@yandex.ru>
2021-06-19 23:30:08 +0300

    Перевод маршрута destroy на исполнение по AJAX Все нотификации удаления - через Broadcast + Toastr Все систетные нотификации - через Toastr

---
+++ app/Rules/OGRNControlSumRule.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Rules;
+
+use Illuminate\Contracts\Validation\Rule;
+use Illuminate\Http\Request;
+
+class OGRNControlSumRule implements Rule
+{
+    protected Request $request;
+
+    /**
+     * Create a new rule instance.
+     * @param Request $request
+     */
+    public function __construct(Request $request)
+    {
+        $this->request = $request;
+    }
+
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        // Алгоритм
+        // https://www.sberbank.ru/ru/s_m_business/pro_business/ogrn-i-ogrnip-chto-eto-takoe-i-kak-proverit-ogrn-po-inn/
+        $baselen = strlen($value);
+        $common = substr($value, 0, $baselen - 1);
+        $control = substr($value, $baselen - 1, 1);
+        //
+        $step1 = intval($common) / ($baselen - 2);
+        $step2 = round($step1) * ($baselen - 2);;
+        $step3 = (intval($common) - $step2) % 10;
+        $step4 = ($step3 == $control);
+
+        return $step4;
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
+    {
+        return 'Проверьте ОГРН / ОГРНИП - контрольная сумма неверна';
+    }
+}
